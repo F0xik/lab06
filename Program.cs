e991fc6871e31b498eb996e0a4a800ed0f72c6e4
Foxik <tanasov0672@mail.ru>
2023-03-15 19:46:48 +0300

    lab06

--- Program.cs
+++ Program.cs
@@ -1,43 +1,26 @@
 ﻿using System.IO;
-
+using System.Linq;
 Console.WriteLine("На оценку 3"); Console.WriteLine(" ");
 Console.WriteLine("Введите число: ");
 string num;
 num = Console.ReadLine();
 int[] nums = new int[num.Length];
 int i = 0;
-int sum = 0;
-int umn = 1;
+int sum = 0; // сумма
+int umn = 1; // умножение
 foreach (char n in num)
 {
     nums[i] = Convert.ToInt32(n.ToString()); i++;
 }
 Console.WriteLine(string.Join(" ", nums));
-for (int up = 0; up < nums.Length; up++)
+for (int up = 0; up < nums.Length; up++) // сумма и умножение
 {
     sum += nums[up]; umn *= nums[up];
 }
+int max = nums.Max();  // Макc знач
+int min = nums.Min();   // Мин знач
 Console.WriteLine($"Сумма чисел: {sum}" + " " +
     $"Произведение чисел: {umn} ");
+Console.WriteLine($"Максимальное значение: {max}" + " " + $"Минимальное значение: {min}");
 
-Console.WriteLine(" ");
-Console.WriteLine(" "); Console.WriteLine("На оценку 4");
-Console.WriteLine(" ");
-
-
-Console.WriteLine("Введите число: ");
-string num1;
-num1 = Console.ReadLine();
-int[] nums1 = new int[num1.Length];
-int i1 = 0;
-Console.WriteLine("Введите второе число: ");
-string num2;
-num2 = Console.ReadLine();
-int[] nums2 = new int[num2.Length];
-int i2 = 0;
-
-
-
-Console.WriteLine(String.Join("",nums1,"",nums2));
- 
 
